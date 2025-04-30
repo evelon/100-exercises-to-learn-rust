bfec3fa5e7f5be4228b0400ce4e4c936e2d403f8
Jonghyun Lim <ezemango@gmail.com>
2025-05-01 02:32:06 +0900

    pass 04_traits/13_drop

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,28 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+struct DropBomb {
+    flag: bool,
+}
+
+impl DropBomb {
+    pub fn new() -> DropBomb {
+        DropBomb { flag: false }
+    }
+
+    pub fn defuse(&mut self) {
+        self.flag = true
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if !self.flag {
+            panic!("bye")
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
